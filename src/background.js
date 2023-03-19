a357d196262859f9eecc1b399e6605dcde6983ee
Tim <timjhowitz@gmail.com>
2023-03-19 21:31:00 +0000

    Updated background.js to use red colored icon instead of blue if extension is off instead of using badge text

--- src/background.js
+++ src/background.js
@@ -39,12 +39,17 @@ function attemptNavigation(nextState, currentUrl, tabId) {
   }
 }
 
-function saveState(newState, tabId) {
-  chrome.action.setBadgeText({
-    tabId,
-    text: newState.toUpperCase(),
-  });
+function updateIcon(newState, tabId) {
+  if (newState) {
+    chrome.action.setIcon({
+      path: `assets/img/16x16${newState === "off" ? "-disabled" : ""}.png`,
+      tabId,
+    });
+  }
+}
 
+function saveState(newState, tabId) {
+  updateIcon(newState, tabId);
   chrome.storage.sync.set({ extensionEnabledState: newState });
 }
 
@@ -53,10 +58,7 @@ function updateTabToCurrentState(currentUrl, tabId) {
     const currentState = result.extensionEnabledState;
 
     attemptNavigation(currentState, currentUrl, tabId);
-    chrome.action.setBadgeText({
-      tabId,
-      text: currentState.toUpperCase(),
-    });
+    updateIcon(currentState, tabId);
   });
 }
 
@@ -96,14 +98,9 @@ chrome.action.onClicked.addListener(async (tab) => {
   const prevState = result.extensionEnabledState;
 
   // the button only toggles
-  let nextState;
-  if (prevState === "on") {
-    nextState = "off";
-  } else if (prevState === "off") {
-    nextState = "on";
-  }
+  if (prevState) {
+    const nextState = prevState === "on" ? "off" : "on";
 
-  if (nextState) {
     attemptNavigation(nextState, tab.url, tab.id);
     saveState(nextState, tab.id);
   }
